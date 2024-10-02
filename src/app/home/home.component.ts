18dd7b0c214dc24b7ec3921089593a440a3546a5
shri29s <shricharan.r24@iiits.in>
2024-10-02 09:54:19 +0530

    Added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { startup } from '../models/startup';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  startup: startup = {};
 
+  onSubmit() {
+  }
 }
---
+++ src/app/models/startup.ts
@@ -0,0 +1,6 @@
+export interface startup {
+    id?: number;
+    name?: string;
+    address?: string;
+    email?: string;
+}
