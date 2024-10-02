6bd22a5e4ef731a1e0a36e73f0966141b0d6928d
shri29s <shricharan.r24@iiits.in>
2024-10-02 18:34:30 +0530

    jkl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartupdisplayComponent } from './startupdisplay/startupdisplay.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'admin', component: HomeComponent },
   { path: 'startups', component: StartupdisplayComponent },
+  { path: '', component: UserComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
