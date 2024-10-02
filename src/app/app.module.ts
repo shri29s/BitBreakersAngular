f15a0e8683bc4cc08f6e8fcea18f263fb97f64fc
shri29s <shricharan.r24@iiits.in>
2024-10-02 12:15:13 +0530

    Added many components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { StartupdisplayComponent } from './startupdisplay/startupdisplay.component';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    StartupdisplayComponent
+    StartupdisplayComponent,
+    NavComponent,
+    UserComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
